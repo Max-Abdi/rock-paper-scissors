ab3adb6434e216559bdfbd58159bf69146856d72
Max-Abdi <Mohamed_awil@outlook.com>
2024-08-20 11:18:06 +0300

    Find here the solution

--- challenge.js
+++ challenge.js
@@ -1,2 +1,12 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+function playGame(player1, player2) {
+    //logical
+    if (player1 === "rock" && player2 === "scissors") {
+      return "Player 1 is the winner";
+    }
+  
+  
+  }
+  
+  const winner = playGame("rock", "scissors");
+  
+  console.log(winner);
